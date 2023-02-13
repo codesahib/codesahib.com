eda141db75972e78fa0da2f4bef7565f4f141070
codesahib <codesahib@gmail.com>
2023-02-14 00:28:39 +0530

    Temporarily disabling mongoDB and changing start file name

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+// Connect the DB and start server from here
+
+import app from "./server.js"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+import udemyCoursesDAO from "./dao/udemyCoursesDAO.js"
+
+// Include variables from .env
+dotenv.config({path : './config.env'})
+
+const port = process.env.PORT || 8000
+
+// Connecting DB
+app.listen(port, ()=>{
+    console.log(`[index] Listening on port ${port}`)
+})
+// mongoose.connect(process.env.PROJECTS_DB_URI,{ useNewUrlParser: true })
+// .then(async (client)=>{
+//     console.log("[index] Database Connection Successful");
+//     await udemyCoursesDAO.injectDB(client);
+    
+// })
+// .catch((err)=> {
+//     console.log("[index] Error Connecting to DB. Error: "+ err)
+//     process.exit(1)
+// })
