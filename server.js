bda588f70dd7a5be200c4516bf846ade252c3a26
codesahib <codesahib@gmail.com>
2023-03-06 13:36:41 +0530

    committing build folder

--- server.js
+++ server.js
@@ -12,22 +12,23 @@ app.set('view engine', 'ejs') // View Engine for dynamic rendering
 app.use(cors()) // Middleware - Things used by express
 app.use(express.json()) // This is same as BodyParser. It's now included in express. Server can accept JSON in body of request
 
-/* [For backend testing only] Allow home page rendering from frontend */
+/* [For backend testing only] Allow home page rendering from frontend 
 app.get('/',(req,res) => {
   res.send("Something's cooking")
 });
 app.get('/api',(req,res) => {
   res.send('API home')
 });
+*/
+
+app.use(express.static(path.join(__dirname,"frontend/build"))) // Use static frontend
+
+app.use("/api/v1/udemy_courses", udemy_courses) // General procedure for api routes
 
 app.use("*", (req,res) => {
   res.status(404).json({error: "[server] Page not found"})
 }) // Or app.all('*',(req,res) => {res.status(404).send('Not Found')})
 
-
-// app.use(express.static(path.join(__dirname,"../frontend/build"))) // Use static frontend
-
-app.use("/api/v1/udemy_courses", udemy_courses) // General procedure for api routes
 
 // app.get("*", function (request, response) {
 //   response.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
